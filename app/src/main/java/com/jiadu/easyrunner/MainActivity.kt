5854f0b7555904f40fd46a210ec0ffccdcad12d5
walker <597222089@qq.com>
2019-07-21 14:38:39 +0800

    自定义尺寸和内部布局

--- app/src/main/java/com/jiadu/easyrunner/MainActivity.kt
+++ app/src/main/java/com/jiadu/easyrunner/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.jiadu.easyrunner
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.jiadu.easyrunner.hencode.custom_size_layout.CustomSizeLayoutActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , View.OnClickListener{
+
+    override fun onClick(v: View?) {
+
+        when (v?.id) {
+            R.id.btn_custom_size ->
+                startActivity(Intent(this, CustomSizeLayoutActivity::class.java))
+        }
+
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btn_custom_size).setOnClickListener(this)
+
     }
+
+
 }
